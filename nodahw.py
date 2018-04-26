5e1b6a5b0f0667d14f28bf1822a323cab4dde012
nodasaul <nodasaul@gmail.com>
2018-04-26 14:53:39 -0400

    Add files via upload

---
+++ nodahw.py
@@ -0,0 +1,23 @@
+from pyspark import SparkContext
+
+if __name__ == '__main__':
+    sc = SparkContext()
+
+    rest = sc.textFile('/data/share/bdm/nyc_restaurants.csv', use_unicode=False).cache()
+    # rest = sc.textFile('nyc_restaurants.csv', use_unicode=False).cache()
+
+    def extract(partId, lines):
+    if partId == 0:
+        next(lines)
+    import csv
+    for row in csv.reader(lines):
+        yield (row[0], (row[7], 1))
+
+rdd.mapPartitionsWithIndex(extract) \
+    .reduceByKey(lambda x, y: x) \
+    .values() \
+    .reduceByKey(lambda x, y: x + y) \
+    .sortBy(lambda x: -x[1]) \
+    .take(25)
+
+count = restaurants.reduceByKey(lambda x, y: x + y).saveAsTextFile("output")
